266c8fa41dc115f8325b46daac0a613a01fc8924
karenbouissa <karenbouissamonica@gmail.com>
2023-11-27 12:19:00 +0100

    correction des bugs

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,97 @@
+// Définition des slides du carrousel
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-console.log(slides);
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et événements</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
-// Récupèrer tous les éléments du carroussel
+// Slide actuel
+let currentSlide = 0;
 
+// Nombre total de slides
+const totalSlides = slides.length;
+
+// Affichage initial du premier slide et des points (dots)
+createSlides(currentSlide);
+dotsUpdate();
+
+// Sélection des éléments du DOM
+const banner = document.getElementById("banner");
 const bannerImg = document.querySelector(".banner-img");
-const precedent = document.querySelector(".arrow_left");
-const suivant = document.querySelector(".arrow_right");
-const dots = document.querySelector(".dot")
-let currentIndex = 0; // Un compteur qui permet de savoir sur quelle slide on se trouve
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const dots = document.querySelectorAll(".dot");
+const dotSelected = document.querySelectorAll(".dot.dot_selected");
 
-// Fonction pour mettre à jour les points indicateurs, l'image et le texte
-function updateCarousel(index, direction) {
-	//correction du bug pour la première et la dernière image
-	if (currentIndex === -1 && direction === 'left') {
-	  currentIndex = slides.length - 1;
-  } else if (currentIndex === slides.length && direction === 'right') {
-	  currentIndex = 0;
+// Mise en place d'event listener sur les flèches gauche et droite
+arrowLeft.addEventListener("click", function () {
+  // Vérification si on est sur le premier slide
+  if (currentSlide === 0) {
+    // Si oui, passer au dernier slide
+    currentSlide = totalSlides - 1;
+  } else {
+    // Sinon, passer au slide précédent
+    currentSlide--;
   }
+  // Mise à jour des slides
+  createSlides(currentSlide);
+});
 
-  // MAJ images
-  const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
-  bannerImg.src = imagePath;
-  bannerImg.alt = `Slide ${currentIndex + 1}`;
+arrowRight.addEventListener("click", function () {
+  // Vérification si on est sur le dernier slide
+  if (currentSlide === totalSlides - 1) {
+    // Si oui, passer au premier slide
+    currentSlide = 0;
+  } else {
+    // Sinon, passer au slide suivant
+    currentSlide++;
+  }
+  // Mise à jour des slides
+  createSlides(currentSlide);
+});
 
-  // MAJ du texte
-  const tagLine = slides[currentIndex].tagLine;
-  document.querySelector('p').innerHTML = tagLine;
-
-  console.log(`Clic sur la flèche ${direction}`);
+// Fonction pour mettre à jour l'état des points (dots)
+function dotsUpdate() {
+  const dotsList = document.querySelectorAll(".dot");
+  for (let index = 0; index < dotsList.length; index++) {
+    const dot = dotsList[index];
+    // Vérification si le point correspond au slide actuel
+    if (index === currentSlide) {
+      // Si oui, le marquer comme sélectionné
+      dot.classList.add("dot_selected");
+    } else {
+      // Sinon, le désélectionner
+      dot.classList.remove("dot_selected");
+    }
+  }
 }
 
-// Gestionnaire d'événement pour le clic sur la flèche gauche
-precedent.addEventListener('click', function () {
-  currentIndex = (currentIndex - 1);
-  updateCarousel(currentIndex, 'left');
-  updateDots(currentIndex); // Mettez à jour les points indicateurs
-});
+// Fonction pour créer et afficher les slides
+function createSlides(currentSlide) {
+  // Récupération des données du slide actuel
+  const element = slides[currentSlide];
+  // Mise à jour de l'image du banner
+  const img = document.querySelector(".banner-img");
+  img.setAttribute("src", "./assets/images/slideshow/" + element.image);
 
-// Gestionnaire d'événement pour le clic sur la flèche droite
-suivant.addEventListener('click', function () {
-  currentIndex = (currentIndex + 1) ;
-  updateCarousel(currentIndex, 'right');
-  updateDots(currentIndex); // Mettez à jour les points indicateurs
-});
+  // Mise à jour du texte du banner
+  const p = document.querySelector(".banner-txt");
+  p.innerHTML = element.tagLine;
+
+  // Mettre à jour de l'état des points (dots)
+  dotsUpdate();
+}
